3fe28ed2d048d1713c5c905d90735325ffcbcc0c
diivanovsky <diivanovskt@gmai.com>
2023-07-15 00:46:49 +0300

    2 task

--- main.py
+++ main.py
@@ -55,3 +55,42 @@ class Storage:
             return new_store
         else:
             return None
+
+
+# 2 Task
+class BeeElephant:
+    def __init__(self, bee, elephant):
+        self.bee = bee
+        self.elephant = elephant
+
+    def fly(self):
+        if self.bee >= self.elephant:
+            return True
+        return False
+
+    def trumpet(self):
+        if self.bee <= self.elephant:
+            return 'tu-tu-doo-doo!'
+        return 'bzzzzzz'
+
+    def eat(self, meal, value):
+        if meal == 'nectar':
+            self.bee += value
+            self.elephant -= value
+        elif meal == 'grass':
+            self.bee -= value
+            self.elephant += value
+        if self.elephant > 100:
+            self.elephant = 100
+        elif self.elephant < 0:
+            self.elephant = 0
+        if self.bee > 100:
+            self.bee = 100
+        elif self.bee < 0:
+            self.bee = 0
+        return f"Bee's part is {self.bee} and elephant's part is {self.elephant}"
+
+
+# 3 Task
+
+
